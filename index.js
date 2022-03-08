bf1b286924136abb1741360459c4c61987ac9734
shivamsoni <shivam.spirite3@outlook.com>
2022-03-08 23:42:15 +0530

    this is my first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+console.log("hello");
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+let users = [
+	{
+		firstName: "shivam",
+		lastName: "soni",
+		mobile: "5345425",
+		isSaved: true,
+		id: 0,
+	},
+];
+
+const generateId = () => {
+	const maxId = users.length > 0 ? Math.max(...users.map((n) => n.id)) : 0;
+	return maxId + 1;
+};
+
+app.post("/api/users", (request, response) => {
+	console.log("request", request);
+	const body = request.body;
+
+	if (!body) {
+		return response.status(400).json({
+			error: "user is missing",
+		});
+	}
+
+	const user = {
+		firstName: body.firstName,
+		lastName: body.lastName,
+		mobile: body.mobile,
+		isSaved: body.isSaved,
+		id: generateId(),
+	};
+
+	users = users.concat(user);
+	response.json(user);
+});
+
+app.get("/api/users", (request, response) => {
+	response.json(users);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
